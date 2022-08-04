e992d659f78138f8cb2bb3a31177aad1cc473644
Vvfu2011 <91229625+Vvfu2011@users.noreply.github.com>
2022-08-04 10:11:47 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+
+var recognition = new SpeechRecognition();
+
+function start()
+{
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+}
+
+recognition.onresult = function(event) {
+
+    console.log(event);
+
+    var Content = event.results[0][0].trancript;
+    console.log(Console);
+
+    document.getElementById("textbox"),innerHTML =  Content;
+}
